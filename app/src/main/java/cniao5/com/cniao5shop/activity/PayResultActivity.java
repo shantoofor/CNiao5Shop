7d7504d9aed0dca774cc29bc706c2fe6cc4c337d
developer_shantoo@126.com <admin1314>
2017-12-03 22:27:01 +0800

    第一次提交

---
+++ app/src/main/java/cniao5/com/cniao5shop/activity/PayResultActivity.java
@@ -0,0 +1,19 @@
+package cniao5.com.cniao5shop.activity;
+
+import android.os.Bundle;
+
+import cniao5.com.cniao5shop.R;
+
+public class PayResultActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pay_result);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+}
